095c121c1635418506fa0e02b461391a2fff1656
sanjay-jyani <sanjayjyani333@gmail.com>
2020-08-08 09:17:52 +0530

    Initial Commit

---
+++ src/app/post/post-list/post-list.component.ts
@@ -0,0 +1,55 @@
+import {Component, OnInit, OnDestroy} from '@angular/core';
+import {Post} from '../post.model';
+import { PostService } from '../post.service';
+import {Subscription} from 'rxjs';
+import { PageEvent } from '@angular/material/paginator';
+@Component({
+    selector: 'app-post-list',
+    templateUrl: './post-list.component.html',
+    styleUrls: ['./post-list.component.css']
+})
+export class PostListComponent implements OnInit, OnDestroy {
+    // Posts = [
+    //     {title: 'First Post', content: 'This is the first post\'s content' },
+    //     {title: 'Second Post', content: 'This is the second post\'s content' },
+    //     {title: 'Third Post', content: 'This is the third post\'s content' },
+    // ];
+    posts: Post[] = [];
+    isLoading = false;
+    totalPosts = 0;
+    currentPage = 1;
+    postsPerPage = 2;
+    pageSizeOptions = [1, 2, 5, 10];
+    private postsSub : Subscription;
+
+    constructor(public postService: PostService){}
+
+    ngOnInit(){
+        this.isLoading = true;
+        this.postService.getPosts(this.postsPerPage, this.currentPage);
+        this.postsSub = this.postService.getPostUpdateListener()
+        .subscribe((postData: {posts: Post[], postCount: number}) => {
+            this.isLoading = false;
+            this.totalPosts = postData.postCount;
+            this.posts = postData.posts;
+        });
+    }
+
+    onChangedPage(pageData: PageEvent){
+      this.isLoading = true;
+      this.currentPage = pageData.pageIndex + 1;
+      this.postsPerPage = pageData.pageSize;
+      this.postService.getPosts(this.postsPerPage, this.currentPage);
+    }
+
+    onDelete(postId: string){
+      this.isLoading = true;
+        this.postService.deletePost(postId).subscribe(() => {
+          this.postService.getPosts(this.postsPerPage, this.currentPage);
+        });
+    }
+
+    ngOnDestroy(){
+        this.postsSub.unsubscribe();
+    }
+}
